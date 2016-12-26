63112baa5791e36a17229ce42bbe257d76dfe8f9
Bill Westfall <billwestfall@yahoo.com>
2016-12-25 16:47:35 -0800

    Update loop.rb

--- scripts/loop.rb
+++ scripts/loop.rb
@@ -2,7 +2,7 @@
 
 Dir.foreach('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/*') do |item|
   next if item == '*.p'
-  /home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr < item.p | prover9 > item.out
+  /home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr < #{item}.p | prover9 > #{item}.out
   print "complete\n"
   sleep 10
 end
