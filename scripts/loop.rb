9eda634f6292ed46662b48acb2f56b28bdba15c0
Bill Westfall <billwestfall@yahoo.com>
2016-12-25 17:18:59 -0800

    Update loop.rb

--- scripts/loop.rb
+++ scripts/loop.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/ruby
 
-#Dir.foreach('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/*') do |item|
-#  next if item == '*.p'
-#  /home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr < #{item}.p | prover9 > #{item}.out
-#  print "complete\n"
-#  sleep 10
-#end
-
-Dir.glob('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT/*.p').each do |fileName|
-  "'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT/fileName > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in"
-  "'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.out"
+Dir.foreach('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT') do |item|
+  next if item == '*.p'
+  "'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT/#{item} | /home/pi/Documents/LADR-2009-11A/bin/prover9 > #{item}.out
   print "complete\n"
   sleep 10
 end
+
+#Dir.glob('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT/*.p').each do |fileName|
+#  "'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT/fileName > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in"
+#  "'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.out"
+#  print "complete\n"
+#  sleep 10
+#end
