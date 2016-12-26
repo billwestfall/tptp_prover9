14be6155caedbae129106c815ec59b53cbaff3fb
Bill Westfall <billwestfall@yahoo.com>
2016-12-25 17:15:05 -0800

    Update loop.rb

--- scripts/loop.rb
+++ scripts/loop.rb
@@ -7,8 +7,8 @@
 #  sleep 10
 #end
 
-Dir.glob('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/AGT/*.p').each do |fileName|
-  "'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < fileName > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in"
+Dir.glob('/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/*/*.p').each do |fileName|
+  "'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/*/fileName > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in"
   "'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/fileName.out"
   print "complete\n"
   sleep 10
