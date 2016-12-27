d0b2ec4758b180b56588f280f7537cea6ae4c048
Bill Westfall <billwestfall@yahoo.com>
2016-12-26 18:21:37 -0800

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -7,9 +7,9 @@ sleep 3
 echo "The randomly-selected file is: $path"
 cd "/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL"
 sleep 3
-'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$(file) > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$(file).in
+`'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file.in`
 ls /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/*.in
 sleep 3
-'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$(file).in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$(file).out
+`'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$file.out`
 ls /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles
 sleep 3
