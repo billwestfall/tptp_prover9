6649c496aeece1482ab439d55da7de2fad16843b
Bill Westfall <billwestfall@yahoo.com>
2016-12-26 16:35:03 -0800

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+now="date +'%s'"
 dir='/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL'
 file=`/bin/ls -R -1 "$dir" | sort --random-sort | head -1`
 path=`readlink --canonicalize "$dir/$file"` # Converts to full path
@@ -8,10 +9,10 @@ cd "/home/pi/Documents/tptp/TPTP-v6.4.0"
 cp $path .
 ls
 sleep 3
-"'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < $file > /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles/$file.in"
+"'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < $file > /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles/$now.in"
 ls /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles
 sleep 3
-"'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles/$file.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$file.out"
+"'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles/$now.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$now.out"
 ls /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles
 rm -f *.p
 ls
