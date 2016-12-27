1cd77d12e37c3f46d09a1bb25b1b23f3b6bbdad8
Bill Westfall <billwestfall@yahoo.com>
2016-12-26 16:14:55 -0800

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dir='/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/*/'
-file=`/bin/ls -1 "$dir" | sort --random-sort | head -1`
+dir='/home/pi/Documents/tptp/TPTP-v6.4.0/Problems'
+file=`/bin/ls -R -1 "$dir" | sort --random-sort | head -1`
 path=`readlink --canonicalize "$dir/$file"` # Converts to full path
 echo "The randomly-selected file is: $path"
