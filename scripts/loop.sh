6058ca77af7f0af0c0072819a2e44a75bab6d9bd
Bill Westfall <billwestfall@yahoo.com>
2016-12-26 16:32:48 -0800

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -4,9 +4,15 @@ dir='/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL'
 file=`/bin/ls -R -1 "$dir" | sort --random-sort | head -1`
 path=`readlink --canonicalize "$dir/$file"` # Converts to full path
 echo "The randomly-selected file is: $path"
+cd "/home/pi/Documents/tptp/TPTP-v6.4.0"
 cp $path .
 ls
 sleep 3
+"'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < $file > /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles/$file.in"
+ls /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles
+sleep 3
+"'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Infiles/$file.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$file.out"
+ls /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles
 rm -f *.p
 ls
 sleep 3
