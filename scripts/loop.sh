7a19ad5cf668342775a08e0908b69cb1235c0f45
Bill Westfall <billwestfall@yahoo.com>
2016-12-28 19:47:16 -0800

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -10,10 +10,10 @@ do
   echo "The randomly-selected file is: $path"
   cd "/home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL"
   sleep 3
-  `'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file.in` & sleep 900 ; kill $!
+  `'/home/pi/Documents/LADR-2009-11A/bin/tptp_to_ladr' < /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file > /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file.in & sleep 900 ; kill $!`
   ls /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/*.in
   sleep 3
-  `'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$file.out` & sleep 900 ; kill $!
+  `'/home/pi/Documents/LADR-2009-11A/bin/prover9' -f /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/$file.in > /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles/$file.out & sleep 900 ; kill $!`
   ls /home/pi/Documents/tptp/TPTP-v6.4.0/Problems/ALL/*.in
   ls /home/pi/Documents/tptp/TPTP-v6.4.0/Outfiles
   sleep 900
